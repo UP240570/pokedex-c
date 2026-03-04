3859ddcb8d38e4e5efb8c4ceca13f11248980aab
Andres Rodriguez <up240570@alumnos.upa.edu.mx>
2026-03-04 14:58:50 -0600

    show pokemons on screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,10 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
+
+
+  const [results, setResults] = useState<any[]> ([]);
   useEffect(() => {
     console.log("Entre en pantalla");
     getPokemons();
@@ -9,24 +12,27 @@ export default function Index() {
 
   const getPokemons = async () => {
     try {
-      const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
-      const response = await fetch(URL, {
-        method: "GET",
-      });
+    const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
+    const response = await fetch(URL, {   //fetch jalar informacion
+      method: "GET",
+    }); 
 
-      if (response.ok) {
-        console.log("Request OK");
-      } else {
-        console.log("Bard request");
-      }
-    } catch (error) {
-      console.log("Error en la peticion")
+    if (response.ok) {
+      const data = await response.json();  //json convertir a objeto
+      setResults(data.results);
+    } else {
+      console.log("Bad request"); 
     }
-  };
+  } catch (error) {
+    console.log("Ocurrió un error");
+  }
+}; 
 
   return (
     <View>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      {results.map((item)=>{
+        return <Text key = {item.name}>{item.name}</Text>
+      })}
     </View>
   );
 }
